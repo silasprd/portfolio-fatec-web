cea38fa8788e44443bc1f885c51e95c746e98ff2
silasprd <silas.prado98@gmail.com>
2024-07-23 15:15:54 -0300

    created intro

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
+import "./style.css"
 </script>
 
 <template>
-  <div>
-    <h1>Portfólio Fatec Banco de Dados</h1>
+  <div class="container">
+    <div class="person">
+      <figure>
+        <img src="/src/assets/person-icon.png" alt="person-icon">
+      </figure>
+    </div>
+    <div class="intro">
+      <h1>Silas Rafael Barreto Prado</h1>
+      <h2>Portfólio de Aprendizagem por Projetos Integrados (API)</h2>
+      <h3>FATEC - Banco de Dados</h3>
+      <br>
+      <p>Este portfólio foi criado para apresentar os projetos desenvolvidos ao longo da minha trajetória acadêmica na FATEC, com ênfase na Aprendizagem por Projetos Integrados (API). O objetivo é destacar a aplicação prática dos conhecimentos adquiridos durante os semestres, demonstrando a capacidade de resolver problemas reais e complexos através de abordagens multidisciplinares. Cada projeto reflete o aprendizado e as habilidades adquiridas em diferentes áreas do conhecimento, evidenciando a integração entre teoria e prática.</p>
+      <br>
+      <div class="semesters">
+        <div class="semesters-box">1° SEMESTRE</div>
+        <div class="semesters-box">2° SEMESTRE</div>
+        <div class="semesters-box">3° SEMESTRE</div>
+        <div class="semesters-box">4° SEMESTRE</div>
+      </div>
+    </div>
   </div>
 </template>
-
-<style scoped>
-
-</style>
