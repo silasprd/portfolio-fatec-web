371d47c0986fdb4cf5d4909a42701249c3b8bca0
Silas Prado <silas.prado98@gmail.com>
2024-12-10 01:29:54 -0300

    add academic and professional experiences

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,9 +4,9 @@ import "../style.css"
 
 <template>
   <div class="container">
-    
+
     <div class="intro">
-      
+
       <div class="name">
         <i class="fa-solid fa-circle-user"></i>
         <h1>Silas Rafael Barreto Prado</h1>
@@ -27,8 +27,11 @@ import "../style.css"
         </div>
       </div>
       <br>
-      <p>Sou aluno de Banco de Dados na Fatec São José dos Campos e atuo como Analista de Desenvolvimento de Sistemas, trabalhando com as tecnologias NodeJS e Angular. Tenho experiência com linguagens como Java, JavaScript, PHP, HTML, CSS e frameworks como Angular, React, Vue, além de Node.js e bancos de dados como MySQL e MongoDB.
-      Estou sempre aprimorando meus conhecimentos em desenvolvimento web e novas tecnologias, com foco em oferecer soluções criativas e eficientes.</p>
+      <p>Sou aluno de Banco de Dados na Fatec São José dos Campos e atuo como Analista de Desenvolvimento de Sistemas,
+        trabalhando com as tecnologias NodeJS e Angular. Tenho experiência com linguagens como Java, JavaScript, PHP,
+        HTML, CSS e frameworks como Angular, React, Vue, além de Node.js e bancos de dados como MySQL e MongoDB.
+        Estou sempre aprimorando meus conhecimentos em desenvolvimento web e novas tecnologias, com foco em oferecer
+        soluções criativas e eficientes.</p>
       <br>
     </div>
     <div class="person">
@@ -40,74 +43,105 @@ import "../style.css"
   <hr style="width: 90%;">
   <br><br>
   <div class="knowledges">
-      <div class="title">
-        <h1>Meus conhecimentos</h1>
-        <i class="fa-solid fa-book"></i>
-      </div>
-      <ul>
-        <li>
-          <img align="center" alt="Java-icon" height="35" width="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg">
-          <p><b>Typescript: </b>Desenvolvi diversos projetos utilizando a linguagem Typescript, e atualmente é a linguagem que possuo um conhecimento mais sólido. Trabalhei com Frameworks como Angular e Vue, desenvolvendo aplicações para monitoramento de embarcações, gestão de usuários etc.</p>
-        </li>
-        <li>
-          <img align="center" alt="Java-icon" height="35" width="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg">
-          <p><b>Java: </b>Colaborei no desenvolvimento de projetos acadêmicos para diversas empresas utilizando Java e SpringBoot. Dentre os projetos estão projetos de gestão de vagas de emprego, sistema gerenciador de vendas e um software de inspeção predial .</p>
-        </li>
-        <li>
-          <img align="center" alt="Java-icon" height="35" width="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/go/go-original-wordmark.svg">
-          <p><b>Go: </b>Desenvolvi pequenos projetos pessoais utilizando a linguagem Go. Dentre eles, um sistema de gerenciamento de veículos com possibilidade de envio de contatos. Atualmente faço pequenos projetos para estudo utilizando a linguagem.</p>
-        </li>
-        <li>       
-          <img align="center" alt="Python-icon" height="35" width="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" />        
-          <p><b>Python: </b>Aprimorei minhas habilidades em python durante meu período na faculdade, onde pude desenvolver algoritmos como exemplos de criptografia e socket. Também fui desafiado a desenvolver um jogo de Batalha Naval reduzido, para matéria de algoritmos, conforme especificações do professor, onde fui contemplado com a nota máxima(10). Você pode conferir este projeto em: <a href="https://github.com/silasprd/naval-battle">https://github.com/silasprd/naval-battle.</a></p>
-        </li>
-        <li>
-          <img align="center" alt="JS-icon" height="35" width="50"
+    <div class="title">
+      <h1>Meus conhecimentos</h1>
+      <i class="fa-solid fa-book"></i>
+    </div>
+    <ul>
+      <li>
+        <img align="center" alt="Java-icon" height="35" width="50"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg">
+        <p><b>Typescript: </b>Desenvolvi diversos projetos utilizando a linguagem Typescript, e atualmente é a linguagem
+          que possuo um conhecimento mais sólido. Trabalhei com Frameworks como Angular e Vue, desenvolvendo aplicações
+          para monitoramento de embarcações, gestão de usuários etc.</p>
+      </li>
+      <li>
+        <img align="center" alt="Java-icon" height="35" width="50"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg">
+        <p><b>Java: </b>Colaborei no desenvolvimento de projetos acadêmicos para diversas empresas utilizando Java e
+          SpringBoot. Dentre os projetos estão projetos de gestão de vagas de emprego, sistema gerenciador de vendas e
+          um software de inspeção predial .</p>
+      </li>
+      <li>
+        <img align="center" alt="Java-icon" height="35" width="50"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/go/go-original-wordmark.svg">
+        <p><b>Go: </b>Desenvolvi pequenos projetos pessoais utilizando a linguagem Go. Dentre eles, um sistema de
+          gerenciamento de veículos com possibilidade de envio de contatos. Atualmente faço pequenos projetos para
+          estudo utilizando a linguagem.</p>
+      </li>
+      <li>
+        <img align="center" alt="Python-icon" height="35" width="50"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" />
+        <p><b>Python: </b>Aprimorei minhas habilidades em python durante meu período na faculdade, onde pude desenvolver
+          algoritmos como exemplos de criptografia e socket. Também fui desafiado a desenvolver um jogo de Batalha Naval
+          reduzido, para matéria de algoritmos, conforme especificações do professor, onde fui contemplado com a nota
+          máxima(10). Você pode conferir este projeto em: <a
+            href="https://github.com/silasprd/naval-battle">https://github.com/silasprd/naval-battle.</a></p>
+      </li>
+      <li>
+        <img align="center" alt="JS-icon" height="35" width="50"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg">
-          <p><b>MySQL: </b>Trabalhei bastante com o banco de dados MySQL, analisando e escrevendo querys para a manipulação de dados, seja diretamente em uma ferramenta de Database Manager utilizando SQL, ou diretamente no código utilizando JPQL.</p>
-        </li>
-        <li>
-          <img align="center" alt="Java-icon" height="35" width="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg">
-          <p><b>MongoDB: </b>Tenho conhecimento na integração do MongoDB com aplicações usando Node.js, Java e Go. Sou familiarizado com operações de CRUD e consultas avançadas. Também tenho experiência na modelagem de dados e criação de esquemas eficientes para armazenar informações.</p>
-        </li>
-      </ul>
+        <p><b>MySQL: </b>Trabalhei bastante com o banco de dados MySQL, analisando e escrevendo querys para a
+          manipulação de dados, seja diretamente em uma ferramenta de Database Manager utilizando SQL, ou diretamente no
+          código utilizando JPQL.</p>
+      </li>
+      <li>
+        <img align="center" alt="Java-icon" height="35" width="50"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg">
+        <p><b>MongoDB: </b>Tenho conhecimento na integração do MongoDB com aplicações usando Node.js, Java e Go. Sou
+          familiarizado com operações de CRUD e consultas avançadas. Também tenho experiência na modelagem de dados e
+          criação de esquemas eficientes para armazenar informações.</p>
+      </li>
+    </ul>
+  </div>
+  <hr style="width: 90%;">
+  <div class="professional-experience">
+    <div class="title">
+      <h1>Experiência Profissional</h1>
+      <i class="fa-solid fa-book"></i>
     </div>
-  <div class="portfolios">
-    <!-- <div>
-      <router-link to="/portfolio-fatec-web/semestre-1">
-        <img src="/src/assets/semester1/boardclass.png" alt="boardclass" width="200">
-      </router-link>
-      <h4>Semestre 1</h4>
+    <ul>
+      <li>
+        <h3>Embeaer S.A</h3>
+        <h4><b>• Aprendiz em Desenvolvimento de Sistemas</b></h4>
+        <h4>08/2019 - 03/2021</h4>
+      </li>
+      <li>
+        <h3>Valltech</h3>
+        <h4><b>• Estágio em Suporte Técnico</b></h4>
+        <h4>04/2022 - 06/2022</h4>
+      </li>
+      <li>
+        <h3>Tell link Engenharia</h3>
+        <h4><b>• Aprendiz de Desenvolvimento de Sistemas</b></h4>
+        <h4>06/2022 - 06/2024</h4>
+        <h4><b>• Analista de Desenvolvimento de Sistemas</b></h4>
+        <h4>06/2024 - 11/2024</h4>
+      </li>
+      <li>
+        <h3>iez! Telecom</h3>
+        <h4><b>• Desenvolvedor Front End</b></h4>
+        <h4>11/2024 - atual</h4>
+      </li>
+    </ul>
+  </div>
+  <hr style="width: 90%;">
+  <div class="academic-experience">
+    <div class="title">
+      <h1>Experiência Acadêmica</h1>
+      <i class="fa-solid fa-book"></i>
     </div>
-    <div>
-      <router-link to="/portfolio-fatec-web/semestre-2">
-        <img src="/src/assets/semester2/image.png" alt="boardclass" width="200">
-      </router-link>
-      <h4>Semestre 2</h4>
-    </div>
-    <div>
-      <router-link to="/portfolio-fatec-web/semestre-3">
-        <img src="/src/assets/semester3/domrock.png" alt="boardclass" width="200">
-      </router-link>
-      <h4>Semestre 3</h4>
-    </div>
-    <div>
-      <router-link to="/portfolio-fatec-web/semestre-4">
-        <img src="/src/assets/semester4/jaia_logo.png" alt="boardclass" width="200">
-      </router-link>
-      <h4>Semestre 4</h4>
-    </div>
-    <div>
-      <router-link to="/portfolio-fatec-web/semestre-5">
-        <img src="/src/assets/semester1/boardclass.jpeg" alt="boardclass" width="200">
-      </router-link>
-      <h4>Semestre 5</h4>
-    </div>
-    <div>
-      <router-link to="/portfolio-fatec-web/semestre-6">
-        <img src="/src/assets/semester1/boardclass.jpeg" alt="boardclass" width="200">
-      </router-link>
-      <h4>Semestre 6</h4>
-    </div> -->
+    <ul>
+      <li>
+        <h3>Senai Santos Dumont</h3>
+        <h4><b>• Técnico em Desenvolvimento de Sistemas</b></h4>
+        <h4>08/2019 - 03/2021</h4>
+      </li>
+      <li>
+        <h3>Fatec Prof. Jessen Vidal</h3>
+        <h4><b>• Tecnólogo em Banco de Dados</b></h4>
+        <h4>02/2022 - 12/2024</h4>
+      </li>
+    </ul>
   </div>
 </template>
